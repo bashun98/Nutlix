7e72186d60df305d0298704143ffbda1be3e0824
bashun98 <bashun98@gmail.com>
2022-04-08 10:58:36 +0300

    Initial Commit

---
+++ Nutlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nutlix
+//
+//  Created by Евгений Башун on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
